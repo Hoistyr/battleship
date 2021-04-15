b00734433c2a75b17351561990466b84f7d3ed3f
Hoistyr <hoistyd@gmail.com>
2021-04-15 17:39:04 -0600

    Added the test/function for placing a ship facing left

--- src/components/gameboardFactory.js
+++ src/components/gameboardFactory.js
@@ -3,35 +3,36 @@ import shipFactory from './shipFactory';
 const gameboardFactory = () => {
   const columnObject = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
   const rowDetails = {
+    hasShip: false,
     ship: null,
     shipPart: '',
     attacked: false,
   }
   const rowObject = {
-    1: rowDetails,
-    2: rowDetails,
-    3: rowDetails,
-    4: rowDetails,
-    5: rowDetails,
-    6: rowDetails,
-    7: rowDetails,
-    8: rowDetails,
-    9: rowDetails,
-    10: rowDetails,
+    1: {...rowDetails},
+    2: {...rowDetails},
+    3: {...rowDetails},
+    4: {...rowDetails},
+    5: {...rowDetails},
+    6: {...rowDetails},
+    7: {...rowDetails},
+    8: {...rowDetails},
+    9: {...rowDetails},
+    10: {...rowDetails},
   };
 
   const gameBoard = {
     squares: {
-      a: rowObject,
-      b: rowObject,
-      c: rowObject,
-      d: rowObject,
-      e: rowObject,
-      f: rowObject,
-      g: rowObject,
-      h: rowObject,
-      i: rowObject,
-      j: rowObject,
+      a: {...rowObject},
+      b: {...rowObject},
+      c: {...rowObject},
+      d: {...rowObject},
+      e: {...rowObject},
+      f: {...rowObject},
+      g: {...rowObject},
+      h: {...rowObject},
+      i: {...rowObject},
+      j: {...rowObject},
     },
    
     placeShip(startingColumn, startingRow, direction, length) {
@@ -39,25 +40,34 @@ const gameboardFactory = () => {
       const squaresWithNewShip = {...this.squares};
       for (let i = 1; i <= length; i++) {
         if (i === 1) {
-          squaresWithNewShip[startingColumn[startingRow]] = {
+          squaresWithNewShip[startingColumn][startingRow] = {
+            hasShip: true,
             ship: ship,
             shipPart: i,
             attacked: false,
           };
         }
-  
+
         if (direction === 'right' && i !== 1) {
-          const nextSquare = 
-          squaresWithNewShip[startingColumn[startingRow]] = {
+          const nextColumn = columnObject[columnObject.indexOf(startingColumn) + i - 1];
+          squaresWithNewShip[nextColumn][startingRow] = {
+            hasShip: true,
+            ship: ship,
+            shipPart: i,
+            attacked: false,
+          };
+        }
+
+        if (direction === 'left' && i !== 1) {
+          const nextColumn = columnObject[columnObject.indexOf(startingColumn) - i - 1];
+          squaresWithNewShip[nextColumn][startingRow] = {
+            hasShip: true,
             ship: ship,
             shipPart: i,
             attacked: false,
           };
         }
       }
-      
-
-
       this.squares = squaresWithNewShip;
     },
     receiveAttack(column, row) {
--- src/tests.js/gameboardFactory.test.js
+++ src/tests.js/gameboardFactory.test.js
@@ -7,17 +7,26 @@ test('gameBoard should be an object', () => {
 
 test('gameBoard should have rows and columns which store data', () => {
   const gameBoard = gameboardFactory();
-  expect(gameBoard.squares.a[1]).toEqual({"attacked": false, "ship": null, "shipPart": ""}
+  expect(gameBoard.squares.a[1]).toEqual({"attacked": false, "hasShip": false, "ship": null, "shipPart": ""}
   );
-  expect(gameBoard.squares.e[5]).toEqual({"attacked": false, "ship": null, "shipPart": ""}
+  expect(gameBoard.squares.e[5]).toEqual({"attacked": false, "hasShip": false, "ship": null, "shipPart": ""}
   );
-  expect(gameBoard.squares.j[10]).toEqual({"attacked": false, "ship": null, "shipPart": ""}
+  expect(gameBoard.squares.j[10]).toEqual({"attacked": false, "hasShip": false, "ship": null, "shipPart": ""}
   );
   
 });
 
-test('gameBoard placeShip should put the ship in the right place', () => {
+test('gameBoard placeShip should put the ship in the right place: Dir: Right', () => {
   const gameBoard = gameboardFactory();
   gameBoard.placeShip('a', 1, 'right', 5);
-  expect(gameBoard.squares.a[3].shipPart).toBe(3);
+  expect(gameBoard.squares.a[1].hasShip).toBe(true);
+  expect(gameBoard.squares.c[1].shipPart).toBe(3);
+});
+
+//TODO Implement the tests for the other directions
+test('gameBoard placeShip should put the ship in the right place: Dir: Left', () => {
+  const gameBoard = gameboardFactory();
+  gameBoard.placeShip('j', 1, 'left', 5);
+  expect(gameBoard.squares.j[1].hasShip).toBe(true);
+  expect(gameBoard.squares.h[1].shipPart).toBe(3);
 });
